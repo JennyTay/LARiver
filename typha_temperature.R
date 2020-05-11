d85f4b2bf60d7b37e0c9af46ea67bd8c8b93c44f
JennyTay <jbtaylor339@gmail.com>
2020-05-11 16:26:03 -0700

    started typha and temperature data cleaning

---
+++ typha_temperature.R
@@ -0,0 +1,12 @@
+#prepping data for temperature impacts on Typha
+
+tmp1 <- read.csv("Typha_Temperature_Data_Collection/Lombardi_etal_1997.csv")
+names(tmp1)
+
+
+tmp2 <- read.csv("Typha_Temperature_Data_Collection/Morinaga1926.csv")
+names(tmp2)
+
+
+tmp3 <- read.csv("Typha_Temperature_Data_Collection/Sifton_2011.csv")
+names(tmp3)
